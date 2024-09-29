d2ea19d9a1376faa4d3bdd4a828ee65f5f2c9396
Etienne Posthumus <eposthumus@gmail.com>
2024-09-29 11:59:18 +0200

    Comments callable

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from fizzysearch import register, rewrite
+from fizzysearch import register, rewrite, comments
 
 
 def test_rewrite_simple():
@@ -16,5 +16,18 @@ def test_rewrite_simple():
     assert rewritten_query == expected_query
 
 
+def test_comment_caller():
+    found = set()
+
+    comments(lambda x: found.add(x))
+    comments(lambda x: found.add(x.lower().replace(" ", "_")))
+
+    query = '# This is a comment\nSELECT ?var WHERE { ?var <http://fizzy/simple> "something" . }'
+    rewritten_query = rewrite(query)
+    assert "# This is a comment" in found
+    assert "#_this_is_a_comment" in found
+
+
 if __name__ == "__main__":
     test_rewrite_simple()
+    test_comment_caller()
